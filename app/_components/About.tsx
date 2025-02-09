88eac35d203526576930715c93f06c5be1567ca2
unknown <saadsyed950@gmail.com>
2025-02-09 19:12:38 +0530

    About section is complete

--- app/_components/About.tsx
+++ app/_components/About.tsx
@@ -4,14 +4,14 @@ const About = () => {
   return (
     <div className="flex justify-center items-center flex-col bg-neutral-900 w-full text-white font-unbounded lg:px-20 px-8">
       <Headings title="About" />
-      <div className="flex justify-center w-full lg:py-10 lg:flex-row">
+      <div className="flex justify-center w-full py-10 flex-col lg:gap-y-10 gap-y-16 lg:flex-row">
         <div className="flex justify-start items-start lg:w-[50%] lg:h-min">
-          <h3 className="lg:text-4xl capitalize tracking-wide">
+          <h3 className="lg:text-4xl text-2xl capitalize tracking-wide">
             Over the years, I have worked on customer-facing products with a
             strong focus on excellent user experience and accessibility
           </h3>
         </div>
-        <div className="flex justify-start items-start lg:flex-col lg:gap-y-8 lg:w-[50%] lg:h-min text-sm font-extralight capitalize tracking-wide text-neutral-300">
+        <div className="flex justify-start items-start flex-col gap-y-8 lg:w-[50%] lg:h-min text-sm font-extralight capitalize tracking-wide text-neutral-300">
           <p>
             Self-taught software engineer with a strong knowledge of TypeScript,
             Python, and Java. I am passionate about working on meaningful
